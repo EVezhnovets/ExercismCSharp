49791f9a1512d6b2224cf7b16a3ba74eb62f45e2
Euheni <eugene.vezhnavets@gmail>
2023-06-09 18:09:33 +0300

    Add PythagoreanTriplet solution

---
+++ PythagoreanTriplet/PythagoreanTriplet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+public static class PythagoreanTriplet
+{
+    public static IEnumerable<(int a, int b, int c)> TripletsWithSum(int n)
+    {
+        var result = new List<(int, int, int)>();
+        for (int a = 1; a < n / 3; a++)
+        {
+            for (int b = a + 1; b <= n / 2; b++)
+            {
+                var c = n - a - b;
+                if (a * a + b * b == c * c)
+                {
+                    result.Add((a, b, c));
+                }
+            }
+        }
+        return result;
+    }
+}
